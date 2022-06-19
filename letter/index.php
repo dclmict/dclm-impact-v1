d67a289e3924fdcbf12ab5a8da656f451a553ef4
goodmuyis <goodmuyis@yahoo.com>
2022-06-19 10:34:31 +0100

    Letter Update

--- letter/index.php
+++ letter/index.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 use Ghostff\TextToImage\Text;
 use Ghostff\TextToImage\TextToImage;
@@ -6,13 +7,15 @@ require_once __DIR__ . '/src/TextToImage.php';
 require_once __DIR__ . '/src/Text.php';
 
 
+
+$script_url = dirname($_SERVER['PHP_SELF']);
+
 // Clearing previous letters generated when you visit
 // https://impact2022.org/letter/?purge=yes
 
 if(isset($_GET['purge'])){
-//	if (date('Hi') == "0300") {
 	if ($_GET['purge'] == "yes") {
-		$files = glob(__DIR__ .'/temp/*');
+		$files = glob(__DIR__ .'/cache/letter/*');
 		foreach($files as $file){
 		  if(is_file($file)) {
 			unlink($file);
@@ -26,7 +29,7 @@ $use_database = 0;
 if(isset($_GET['setlocal']) and $_GET['setlocal'] == 'yes'){
 	$allowLocation = 1;
 }else{
-	$allowLocation = 0;
+	$allowLocation = 1;
 }
 
 if(isset($_GET['setDate']) and $_GET['setDate'] == 'yes'){
@@ -40,48 +43,65 @@ $states = ["Abia","Adamawa","Akwa Ibom","Anambra","Bauchi","Bayelsa","Benue","Bo
 
 if(isset($_POST['getLetter'])){
 
+
+
 	$name = filter_var($_POST['fullname'], FILTER_SANITIZE_STRING);
 	@$location = filter_var($_POST['address'], FILTER_SANITIZE_STRING);
-	@$location2 = filter_var($_POST['address2'], FILTER_SANITIZE_STRING);
-	@$date = filter_var(str_replace('','',$_POST['setDate']), FILTER_SANITIZE_STRING);
+	// @$location2 = filter_var($_POST['address2'], FILTER_SANITIZE_STRING);
+	if(isset($_POST['setDate'])){
+		// @$date = filter_var(str_replace('','',$_POST['setDate']), FILTER_SANITIZE_STRING);
+		@$date = date('l, F d, Y', strtotime($_POST['setDate']));
+		$text4  = Text::from($date.", at ")
+	        ->position(310, 736)
+	        ->font(12, __DIR__ . '/fonts/Raleway-ExtraBold.ttf')
+	        ->color(24, 54, 92);		
+	}
 
 	@$phone = filter_var($_POST['phone'], FILTER_SANITIZE_STRING);
 	@$state = filter_var($_POST['state'], FILTER_SANITIZE_STRING);
 
-	$filename = "/temp/letter4-".str_replace(' ','-', $name);
+	$filename = "/cache/letter/letter4-".str_replace(' ','-', $name);
 
 	$shareURL = $_SERVER['SERVER_NAME'] . $filename .".jpg";
 
-	$text  = Text::from("Dear ".$name.",")
-		        ->position(55, 410)
-		        ->font(24, __DIR__ . '/sweet purple.otf')
-		        ->color(23,62, 95);
 
-	$text4  = Text::from($date.", 2022 at ")
-		        ->position(350, 891)
-		        ->font(16, __DIR__ . '/kalypsa-medium.ttf')
-		        ->color(23,62, 95);
+	function getAddr($location){
+		$str = $location;
+		$str2 = explode('|', wordwrap($str,20,'|'));
+		$addr_1 = $str2[0];
+		unset($str2[0]);
+		$addr_2 = implode(' ', $str2);
+			$address = (object)[
+				'addr_1' => $addr_1,
+				'addr_2' => $addr_2,
+			];
+			return $address;
+	}
 
-	$text2  = Text::from($location)
-		        ->position(570, 891)
-		        ->font(16, __DIR__ . '/kalypsa-medium.ttf')
-		        ->color(23,62, 95);
+	$text  = Text::from($name.",")
+		        ->position(150, 288)
+		        ->font(12, __DIR__ . '/fonts/Raleway-SemiBold.ttf')
+		        ->color(24, 54, 92);
 
-	$text3  = Text::from($location2)
-		        ->position(55, 922)
-		        ->font(16, __DIR__ . '/kalypsa-medium.ttf')
-		        ->color(23,62, 95);
+	$text2  = Text::from(getAddr($location)->addr_2)
+		        ->position(100, 759)
+		        ->font(12, __DIR__ . '/fonts/Raleway-SemiBold.ttf')
+		        ->color(24, 54, 92);
 
+	$text3  = Text::from(getAddr($location)->addr_1)
+		        ->position(540, 736)
+		        ->font(12, __DIR__ . '/fonts/Raleway-SemiBold.ttf')
+		        ->color(24, 54, 92);
 
 
 	if($allowLocation == 1 && $customDateLocal == 1){
-		(new TextToImage(__DIR__ . '/letter3.jpg'))->addTexts($text, $text2, $text3, $text4)->render(__DIR__ . $filename.'.jpg');
+		(new TextToImage(__DIR__ . '/img/impact2-letter-dated.jpg'))->addTexts($text, $text2, $text3, $text4)->render(__DIR__ . $filename.'.jpg');
 	}
 	elseif($allowLocation == 1){
-		(new TextToImage(__DIR__ . '/letter2.jpg'))->addTexts($text, $text2, $text3)->render(__DIR__ . $filename.'.jpg');
+		(new TextToImage(__DIR__ . '/img/impact2-letter.jpg'))->addTexts($text, $text2, $text3)->render(__DIR__ . $filename.'.jpg');
 	}
 	elseif($allowLocation == 0){
-		(new TextToImage(__DIR__ . '/letter.jpg'))->addTexts($text)->render(__DIR__ . $filename.'.jpg');
+		(new TextToImage(__DIR__ . '/img/impact2-letter.jpg'))->addTexts($text)->render(__DIR__ . $filename.'.jpg');
 	}
 
 
@@ -115,7 +135,7 @@ if(isset($_POST['getLetter'])){
 <meta name="description" content="">
 <meta name="author" content="DCLM SMAT">
 <meta name="generator" content="Hugo 0.88.1">
-<title>Invitation to Global Youth Convocation</title>
+<title>Invitation to Global Youth Impact Academy</title>
 
 <!-- Bootstrap core CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -140,7 +160,7 @@ body {
   padding-bottom: 40px;
   background-color: #f5f5f5;
 }
-body {background: #f5f5f5 url(./gs-bg.jpg) no-repeat top right /cover;}
+body {background: #f5f5f5 url(img/gs-bg.jpg) no-repeat top right /cover;}
 
 .bd-placeholder-img {
 font-size: 1.125rem;
@@ -233,31 +253,33 @@ user-select: none;
 	  <form action="" method="POST">
 		<img class="mb-4" src="dclm-logo.png" alt="" width="72">
 		<h1 class="h6 mb-3 fw-bold text-light">Please enter your invitee's details</h1>
-		<?php //print date('Hi') ;?>
+		<?php //print date('Hi'); ?>
 		<div class="form-floating">
 		  <input type="text" class="form-control" id="fullname" name="fullname" placeholder="Fullname">
 		  <label for="fullname">Fullname</label>
 		</div>
 
-		<?if($allowLocation == 1){?>
+		
 		<div class="form-floating mt-1">
-		  <input type="text" class="form-control" id="address" name="address" maxlength="34" placeholder="Address Line 1">
-		  <label for="address">Address Line 1</label>
+		  <input type="text" class="form-control" id="address" name="address" maxlength="180" placeholder="Location Address">
+		  <label for="address">Location Address</label>
 		</div>
+		<?php if($allowLocation == 10){ ?>
 		<div class="form-floating mt-1">
-		  <input type="text" class="form-control" id="address2" name="address2" maxlength="70" placeholder="Address Line 2">
+		  <input type="text" class="form-control" id="address2" name="address2" maxlength="180" placeholder="Address Line 2">
 		  <label for="address2">Address Line 2</label>
 		</div>
-		<? } ?>
+		<?php } ?>
 
-		<?if($customDateLocal == 1){?>
+		<?php if($customDateLocal == 1){ ?>
 		<div class="form-floating mt-1">
-		  <input type="text" class="form-control" id="setDate" name="setDate" maxlength="34" placeholder="Event Date(ex: January 5 - 6)">
-		  <label for="setDate">Event Date(ex: January 5 - 6)</label>
+		  <!-- <input type="date" class="form-control" id="setDate" name="setDate" maxlength="34" placeholder="Event Date(ex: January 5 - 6)"> -->
+		  <input type="date" class="form-control" id="setDate" name="setDate">
+		  <label for="setDate">Event Date</label>
 		</div>
-		<? } ?>
+		<?php } ?>
 
-		<?if($use_database == 1){?>
+		<?php if($use_database == 1){ ?>
 		<div class="form-floating">
 		  <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone Number">
 		  <label for="phone">Phone Number</label>
@@ -266,17 +288,28 @@ user-select: none;
 		<div class="form-floating">
 		  <select class="form-select" id="state" name="state">
 			<option selected disabled>Select State</option>
-			<?php foreach($states as $state){?>
+			<?php foreach($states as $state){ ?>
 				<option><?php print $state;?></option>
-			<? } ?>
+			<?php } ?>
 		  </select>
 		  <label for="state" class="form-label">Select State:</label>
 		</div>
-		<? } ?>
+		<?php } ?>
 		<button name="getLetter" class="w-100 btn btn-lg btn-primary mt-3" type="submit">Submit</button>
-		<a href="./?setlocal=yes" class="btn btn-sm btn-danger mt-1">Get a Letter with Custom Location</a>
-		<a href="./?setlocal=yes&setDate=yes" class="btn btn-sm btn-warning mt-1">Letter with Custom Date & Location</a>
+		<!-- <a href="<?php print $script_url;?>/letter.php?setlocal=yes" class="btn btn-sm btn-danger mt-1">Get a Letter with Custom Location</a> -->
+		<a href="<?php print $script_url;?>/index.php?setlocal=yes&setDate=yes" class="btn btn-sm btn-warning mt-1">Letter with Custom Date & Location</a>
 	  </form>
+	  
+	  <hr class="my-4">
+
+    <p class="text-center mt-3 d-flex">
+        <a href="https://events.dclm.org/personalizedflyer/" class="btn btn-primary btn-sm mr-auto">Global Crusade DP
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="#FFFFFF" d="M13 18v-4h-7v-4h7v-4l6 6-6 6zm-1-16c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12z"/></svg></a>
+      <a href="https://events.dclm.org/personalizedflyer/impact2022.php" class="btn btn-success btn-sm mr-auto">
+          Impact Academy DP
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="#FFFFFF" d="M13 18v-4h-7v-4h7v-4l6 6-6 6zm-1-16c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12z"/></svg></a>                
+    </p> 
+
 	</main>
 
 <?php if(isset($_POST['getLetter'])){ ?>
@@ -292,16 +325,16 @@ user-select: none;
 		</button>
 	  </div>
 	  <div class="toast-body">
-	  	<p class="mx-2">Click on the button below to Download Letter for <b><?php @print $name?></b></p>
-	  	<a href=".<?php print $filename?>.jpg" target="_blank" class="btn btn-success btn-sm mb-1 me-auto" download>Download Letter</a>
+	  	<p class="mx-2">Click on the button below to Download Letter for <b><?php @print $name; ?></b></p>
+	  	<a href=".<?php print $filename; ?>.jpg" target="_blank" class="btn btn-success btn-sm mb-1 me-auto" download>Download Letter</a>
 	  	<span class="btn btn-danger btn-sm m-auto mb-1 closeToast" data-bs-dismiss="toast">Close</span>
 		<div class="letter-box">
-			<img src=".<?php print $filename?>.jpg" class="m-auto border rounded">
+			<img src=".<?php print $filename; ?>.jpg" class="m-auto border rounded">
 		</div>
 	  </div>
 	</div>
 </div><!--//#toastsContainerTopRight-->
-<? } ?>
+<?php } ?>
 
 <script>
 $(document).ready(function(){
@@ -325,15 +358,4 @@ $(document).ready(function(){
 
   </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
 
